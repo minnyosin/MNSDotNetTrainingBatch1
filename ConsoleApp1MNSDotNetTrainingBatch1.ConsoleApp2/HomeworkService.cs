ba5089082f9de86b366c76ee44ec3460cacd27c3
Min Nyo Sin <minnyosin.dev@gmail.com>
2025-04-23 23:28:11 +0630

    Update

---
+++ ConsoleApp1MNSDotNetTrainingBatch1.ConsoleApp2/HomeworkService.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp1MNSDotNetTrainingBatch1.ConsoleApp2
+{
+    internal class HomeworkService
+    {
+        SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch1",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true
+
+        };
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = "select * from Tbl_Homework";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine(dr["No"]);
+                Console.WriteLine(dr["Name"]);
+                Console.WriteLine(dr["GitHubUserName"]);
+                Console.WriteLine("-------------------");
+            }
+        }
+        public void Detail(int no)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = $"select * from Tbl_Homework where No = {no}";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No data found!");
+                return;
+            }
+
+            DataRow dr = dt.Rows[0];
+
+            Console.WriteLine(dr["No"]);
+            Console.WriteLine(dr["Name"]);
+            Console.WriteLine(dr["GitHubUserName"]);
+            Console.WriteLine("-------------------");
+        }
+
+        public void Create(string name, string username)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Homework]
+           ([Name]
+           ,[GitHubUserName])
+     VALUES
+           (@Name,
+           @GitHubUserName)";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@GitHubUserName", username);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public void Update(int no, string name, string username)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = @"UPDATE [dbo].[Tbl_Homework]
+   SET [Name] = @Name
+      ,[GitHubUserName] = @GitHubUserName
+ WHERE No = @No;";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@No", no);
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@GitHubUserName", username);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+        public void Delete(int no)
+        {
+
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = $"delete from Tbl_Homework where No = {no}";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@No", no);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
+
---
+++ ConsoleApp1MNSDotNetTrainingBatch1.ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Data.Common;
+using ConsoleApp1MNSDotNetTrainingBatch1.ConsoleApp2;
+using Microsoft.Data.SqlClient;
+
+HomeworkService homeworkService = new HomeworkService();
+//homeworkService.Read();
+//homeworkService.Detail(1);
+//homeworkService.Create("Soe Htet Lin", "soehtetlin");
+//homeworkService.Update(4, "Aung Thu Lin", "aungthulin21");
+homeworkService.Delete(4);
+
+
