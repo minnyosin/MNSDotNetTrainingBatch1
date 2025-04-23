db3ee6c900b6e82bbc32bb0ec2455e82f9604234
Min Nyo Sin <minnyosin.dev@gmail.com>
2025-04-23 23:33:31 +0630

    Fix Name

--- MNSDotNetTrainingBatch1.FirstConsoleApp2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Net.Http.Headers;
-using MNSDotNetTrainingBatch1.FirstConsoleApp;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Product product = new Product();
